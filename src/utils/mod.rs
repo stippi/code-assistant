e42db3a7d6bd7a89d9307747a7ee7cd6d5f09313
Stephan Aßmus <stephan.assmus@sap.com>
2024-12-02 17:55:20 +0100

    Allow exporting unused imports in src/util/mod.rs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,7 @@ mod command;
 mod file_updater;
 mod utils;
 
+#[allow(unused_imports)]
 pub use command::{CommandExecutor, CommandOutput, DefaultCommandExecutor};
 pub use file_updater::apply_content_updates;
 pub use utils::format_with_line_numbers;
